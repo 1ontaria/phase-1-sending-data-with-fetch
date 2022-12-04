cb8190898c4c14a8a5b6be8f828c368aa9b1953f
Ontaria Woods <ontaria.woods@gmail.com>
2022-12-04 15:00:46 -0500

    commit tree 254cb587c33b917b8d79c6a6c3169c6580ed1179

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Add your code here
+function submitData() {
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      user: "Ontaria",
+      email: "ontaria.woods@gmail.com",
+    })
+      .then((resp) => resp.JSON)
+      .then((object) => console.log(object)),
+  });
+}
